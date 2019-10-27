28f5b16cb5dbd814b4178a8524ed0b106834cb6d
wEather89 <kento.suematsu@gmail.com>
2019-10-27 17:02:05 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'message#index'
 
+  resources :users, only: [:edit, :update]
   resources :prototypes
   resources :messages, only: [:index, :new, :show, :create, :destroy]
   scope module: :prototypes do
