e320bd0910cf962439611f0a7c7f180f8a83b3da
wEather89 <kento.suematsu@gmail.com>
2019-11-02 14:43:54 +0900

    add create and new action to groupscontroller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,17 +1,29 @@
 class GroupsController < ApplicationController
   def new
-    
+    @group = Group.new
+    @group.users << current_user
   end
 
   def edit
-    
+
   end
 
   def create
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'New Group created'
+    else
+      render :new
+    end
   end
 
   def destroy
     
   end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_id: [] )
+  end
+
 end
